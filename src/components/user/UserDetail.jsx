021d80d2dbb469bfb86e4b66f0dd635317c2236e
Quan <trananhquan335@gmail.com>
2023-04-27 14:36:11 +0700

    btvn

--- src/components/user/UserDetail.jsx
+++ src/components/user/UserDetail.jsx
@@ -1,148 +1,257 @@
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate, useParams } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { useEffect, useState } from "react";
 
-function UserDetail({ id }) {
+function UserDetail({}) {
     const [userInfo, setUserInfo] = useState([]);
     const [addressList, setAddressList] = useState([]);
-    const { register, handleSubmit, formState:{ errors } } = useForm();
+    const {
+        register,
+        setValue,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
 
-    const {userId} = useParams();
+    const { userId } = useParams();
 
     useEffect(() => {
         fetch("https://provinces.open-api.vn/api/p/")
-        .then((response) => response.json())
-        .then((json) => setAddressList(json))
-      }, []);
-    
+            .then((response) => response.json())
+            .then((json) => setAddressList(json));
+    }, []);
 
     useEffect(() => {
         fetch(`http://localhost:8080/api/v1/users/${userId}`)
-        .then((response) => response.json())
-        .then((json) => {
-            setUserInfo(json);
-        })
-    }, [])
-    
+            .then((response) => response.json())
+            .then((json) => {
+                setUserInfo(json);
+            });
+    }, []);
 
+    useEffect(() => {
+        if (userInfo.id) {
+            setValue("name", userInfo.name);
+            setValue("email", userInfo.email);
+            setValue("phone", userInfo.phone);
+        }
+    }, [userInfo, setValue]);
 
     const onSubmit = (data) => {
         fetch(`http://localhost:8080/api/v1/${userId}`, {
-        method: "PUT",
-        body: JSON.stringify(data),
-        headers: {
-        "Content-type": "application/json; charset=UTF-8"
-        }
+            method: "PUT",
+            body: JSON.stringify(data),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8",
+            },
         });
         console.log(data);
-    }
+    };
 
     const navigate = useNavigate();
-    
+
     return (
         <>
             <div className="container mt-5 mb-5">
-        <h2 className="text-center text-uppercase mb-3">Thông tin user</h2>
+                <h2 className="text-center text-uppercase mb-3">
+                    Thông tin user
+                </h2>
 
-        <div className="row justify-content-center">
-            <div className="col-md-6">
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <div className="bg-light p-4">
-                    <div className="mb-3">
-                        <label className="col-form-label">Fullname</label>
-                        <input defaultValue={userInfo.name} type="text" id="fullname" className="form-control" 
-                            {...register("name")}
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <label className="col-form-label">Email</label>
-                        <input defaultValue={userInfo.email} type="text" id="email" className="form-control"  
-                            {...register("email")}
-                        
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <label className="col-form-label">Phone</label>
-                        <input defaultValue={userInfo.phone} type="text" id="phone" className="form-control" 
-                            {...register("phone")}
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <label className="col-form-label">Address</label>
-                        <select className="form-select" id="address" 
-                            {...register("address")}
-                        >
-                            <option hidden value="">
-                                Choose province
-                            </option>
-                            {addressList.map((item) => (
-                                <option key={item.code} value={item.name}>
-                                {item.name}
-                                </option>
-                            ))}
-                        </select>
-                    </div>
-                    <div className="mb-3">
-                        <label className="form-label">Avatar</label>
-                        <div className="avatar-preview mb-3 rounded">
-                            <img src="https://via.placeholder.com/200" alt="avatar" id="avatar-preview" className="rounded"/>
-                        </div>
+                <div className="row justify-content-center">
+                    <div className="col-md-6">
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            <div className="bg-light p-4">
+                                <div className="mb-3">
+                                    <label className="col-form-label">
+                                        Fullname
+                                    </label>
+                                    <input
+                                        defaultValue={userInfo.name}
+                                        type="text"
+                                        id="fullname"
+                                        className="form-control"
+                                        {...register("name")}
+                                    />
+                                </div>
+                                <div className="mb-3">
+                                    <label className="col-form-label">
+                                        Email
+                                    </label>
+                                    <input
+                                        defaultValue={userInfo.email}
+                                        type="text"
+                                        id="email"
+                                        className="form-control"
+                                        {...register("email")}
+                                    />
+                                </div>
+                                <div className="mb-3">
+                                    <label className="col-form-label">
+                                        Phone
+                                    </label>
+                                    <input
+                                        defaultValue={userInfo.phone}
+                                        type="text"
+                                        id="phone"
+                                        className="form-control"
+                                        {...register("phone")}
+                                    />
+                                </div>
+                                <div className="mb-3">
+                                    <label className="col-form-label">
+                                        Address
+                                    </label>
+                                    <select
+                                        className="form-select"
+                                        id="address"
+                                        {...register("address")}
+                                    >
+                                        <option hidden value="">
+                                            Choose province
+                                        </option>
+                                        {addressList.map((item) => (
+                                            <option
+                                                key={item.code}
+                                                value={item.name}
+                                            >
+                                                {item.name}
+                                            </option>
+                                        ))}
+                                    </select>
+                                </div>
+                                <div className="mb-3">
+                                    <label className="form-label">Avatar</label>
+                                    <div className="avatar-preview mb-3 rounded">
+                                        <img
+                                            src="https://via.placeholder.com/200"
+                                            alt="avatar"
+                                            id="avatar-preview"
+                                            className="rounded"
+                                        />
+                                    </div>
 
-                        <label className="btn btn-warning" htmlFor="input">
-                            Chọn ảnh
-                        </label>
-                        <input type="file" id="input" className="d-none"/>
+                                    <label
+                                        className="btn btn-warning"
+                                        htmlFor="input"
+                                    >
+                                        Chọn ảnh
+                                    </label>
+                                    <input
+                                        type="file"
+                                        id="input"
+                                        className="d-none"
+                                    />
+                                </div>
+                                <div className="mb-3">
+                                    <label className="col-form-label">
+                                        Password
+                                    </label>
+                                    <div className="">
+                                        <button
+                                            type="button"
+                                            className="btn btn-primary"
+                                            data-bs-toggle="modal"
+                                            data-bs-target="#modal-change-password"
+                                        >
+                                            Đổi mật khẩu
+                                        </button>
+                                        <button
+                                            className="btn btn-warning"
+                                            id="btn-forgot-password"
+                                        >
+                                            Quên mật khẩu
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="text-center mt-3">
+                                <button
+                                    className="btn btn-secondary btn-back"
+                                    onClick={() => navigate("/")}
+                                >
+                                    Quay lại
+                                </button>
+                                <button
+                                    className="btn btn-success"
+                                    type="submit"
+                                    id="btn-save"
+                                >
+                                    Cập nhật
+                                </button>
+                            </div>
+                        </form>
                     </div>
-                    <div className="mb-3">
-                        <label className="col-form-label">Password</label>
-                        <div className="">
-                            <button type="button" className="btn btn-primary" data-bs-toggle="modal"
-                                data-bs-target="#modal-change-password">
-                                Đổi mật khẩu
-                            </button>
-                            <button className="btn btn-warning" id="btn-forgot-password">
-                                Quên mật khẩu
-                            </button>
+                </div>
+
+                {/* Modal đổi mật khẩu */}
+                <div
+                    className="modal fade"
+                    id="modal-change-password"
+                    data-bs-backdrop="static"
+                    data-bs-keyboard="false"
+                    tabIndex="-1"
+                    aria-labelledby="staticBackdropLabel"
+                    aria-hidden="true"
+                >
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5
+                                    className="modal-title"
+                                    id="staticBackdropLabel"
+                                >
+                                    Đổi mật khẩu
+                                </h5>
+                                <button
+                                    type="button"
+                                    className="btn-close"
+                                    data-bs-dismiss="modal"
+                                    aria-label="Close"
+                                ></button>
+                            </div>
+                            <div className="modal-body">
+                                <div className="mb-3">
+                                    <label className="col-form-label">
+                                        Mật khẩu cũ
+                                    </label>
+                                    <input
+                                        type="text"
+                                        id="old-password"
+                                        className="form-control"
+                                    />
+                                </div>
+                                <div className="mb-3">
+                                    <label className="col-form-label">
+                                        Mật khẩu mới
+                                    </label>
+                                    <input
+                                        type="text"
+                                        id="new-password"
+                                        className="form-control"
+                                    />
+                                </div>
+                            </div>
+                            <div className="modal-footer">
+                                <button
+                                    type="button"
+                                    className="btn btn-secondary"
+                                    data-bs-dismiss="modal"
+                                >
+                                    Đóng
+                                </button>
+                                <button
+                                    type="button"
+                                    className="btn btn-primary"
+                                    id="btn-change-password"
+                                >
+                                    Xác nhận
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div className="text-center mt-3">
-                    <button className="btn btn-secondary btn-back" onClick={() => navigate("/")}>Quay lại</button>
-                    <button className="btn btn-success" type = "submit" id="btn-save">Cập nhật</button>
-                </div>
-            </form>
             </div>
-        </div>
-
-        {/* Modal đổi mật khẩu */}
-        <div className="modal fade" id="modal-change-password" data-bs-backdrop="static" data-bs-keyboard="false"
-            tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-            <div className="modal-dialog">
-                <div className="modal-content">
-                    <div className="modal-header">
-                        <h5 className="modal-title" id="staticBackdropLabel">Đổi mật khẩu</h5>
-                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div className="modal-body">
-                        <div className="mb-3">
-                            <label className="col-form-label">Mật khẩu cũ</label>
-                            <input type="text" id="old-password" className="form-control" />
-                        </div>
-                        <div className="mb-3">
-                            <label className="col-form-label">Mật khẩu mới</label>
-                            <input type="text" id="new-password" className="form-control" />
-                        </div>
-                    </div>
-                    <div className="modal-footer">
-                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Đóng</button>
-                        <button type="button" className="btn btn-primary" id="btn-change-password">Xác nhận</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
         </>
-    )
+    );
 }
 
 export default UserDetail;
--- src/components/user/UserList.jsx
+++ src/components/user/UserList.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 let defaultData = [];
 
 function UserList() {
@@ -7,83 +7,90 @@ function UserList() {
     const [searchValue, setSearchValue] = useState("");
 
     useEffect(() => {
-        const newFilter = filterUsers.filter((user,index) => {
+        const newFilter = filterUsers.filter((user, index) => {
             return user.name.toLowerCase().includes(searchValue);
-        })
+        });
         setfilterUsers(newFilter);
         if (searchValue == "") {
             setfilterUsers(defaultData);
         }
-    }, [searchValue])
+    }, [searchValue]);
 
     useEffect(() => {
         fetch("http://localhost:8080/api/v1/users")
-        .then((response) => response.json())
-        .then((json) => {
-            defaultData = json
-            setfilterUsers(json)
-        })
-    }, [])
+            .then((response) => response.json())
+            .then((json) => {
+                console.log(json);
+                defaultData = json;
+                setfilterUsers(json);
+            });
+    }, []);
 
     return (
         <>
             <div className="container mt-5 mb-5">
                 <h2 className="text-center text-uppercase">Danh sách user</h2>
 
-            <div className="row justify-content-center">
-            <div className="col-md-10">
+                <div className="row justify-content-center">
+                    <div className="col-md-10">
+                        <div className="d-flex justify-content-between align-items-center mt-5 mb-4">
+                            <Link to="create" className="btn btn-warning">
+                                {" "}
+                                Tạo user{" "}
+                            </Link>
+                            <input
+                                type="text"
+                                id="search"
+                                className="form-control w-50"
+                                placeholder="Tìm kiếm user"
+                                onChange={(e) => setSearchValue(e.target.value)}
+                            />
+                        </div>
 
-                <div className="d-flex justify-content-between align-items-center mt-5 mb-4">
-                
-                    <Link to ="create" className ="btn btn-warning"> Tạo user </Link>
-                    <input 
-                        type="text" 
-                        id="search" 
-                        className="form-control w-50" 
-                        placeholder="Tìm kiếm user"
-                        onChange={e => setSearchValue(e.target.value)}
-                    />
+                        <div className="bg-light p-4">
+                            <table className="table table-hover">
+                                <thead>
+                                    <tr>
+                                        <th>STT</th>
+                                        <th>Name</th>
+                                        <th>Email</th>
+                                        <th>Phone</th>
+                                        <th>Address</th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
 
+                                <tbody>
+                                    {filterUsers.map((user, index) => (
+                                        <tr key={index}>
+                                            <td>{index + 1}</td>
+                                            <td>{user.name}</td>
+                                            <td>{user.email}</td>
+                                            <td>{user.phone}</td>
+                                            <td>{user.address}</td>
+                                            <td>
+                                                <Link
+                                                    to={"/" + user.id}
+                                                    className="btn btn-success"
+                                                >
+                                                    Xem chi tiết
+                                                </Link>
+                                                <button className="btn btn-danger">
+                                                    Xóa
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+
+                            <p className="message d-none"></p>
+                        </div>
+                    </div>
                 </div>
-
-                <div className="bg-light p-4">
-                    <table className="table table-hover">
-                        <thead>
-                            <tr>
-                                <th>STT</th>
-                                <th>Name</th>
-                                <th>Email</th>
-                                <th>Phone</th>
-                                <th>Address</th>
-                                <th></th>
-                            </tr>
-                        </thead>
-
-                        <tbody>
-                            {filterUsers.map((user, index) => (
-                            <tr key={index}>
-                                <td>{index + 1}</td>
-                                <td>{user.username}</td>
-                                <td>{user.email}</td>
-                                <td>{user.phone}</td>
-                                <td>{user.address}</td>
-                                <td>
-                                    <Link to={"/" + user.id} className="btn btn-success" props={user.id}>Xem chi tiết</Link>
-                                    <button className="btn btn-danger">Xóa</button>
-                                </td>
-                            </tr>
-                            ))}
-                        </tbody>
-                    </table>
-
-                    <p className="message d-none"></p>
-                </div>
-            </div>
-            </div>
             </div>
         </>
-    )
+    );
 }
 
 export default UserList;
-
